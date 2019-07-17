528c9b7fe967e9803fb8305259690ffb45a48ccb
Elias Rahmani <Eliass.Rahmani@gmail.com>
2019-07-17 06:47:35 +0430

    add rest api's

--- src/client/Components/app.tsx
+++ src/client/Components/app.tsx
@@ -4,36 +4,107 @@ import * as ProfilePicture from '../Assets/profile.png';
 
 interface AppStates {
   username?: string;
+  textOfPostTest: string,
+  textForPost: string,
+  textOfPutTest: string,
+  textForPut:string,
+  textOfDeleteTest: string,
+  textForDelete: string,
 }
 export default class App extends React.Component<{}, AppStates> {
-  state: AppStates = { username: null };
-
-  componentDidMount() {
-
-  }
+  state: AppStates = { 
+    username: null,
+    textOfPostTest: '',
+    textForPost: null,
+    textOfPutTest: '',
+    textForPut:null,
+    textOfDeleteTest: '',
+    textForDelete: null,
+   };
 
   getUser = () => {
-    fetch('/api/getUsername')
+    fetch('/api/test')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then(res => this.setState({ username : res.username }));
+  }
+
+
+  sendUserInfo = () => {
+    let text = this.state.textOfPostTest;
+
+    text.trim() && 
+    fetch('/api/test',{
+      method: 'POST',
+      headers: {
+            'Content-Type': 'application/json',
+            //'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+        },
+      body: JSON.stringify({text})})
+    .then(res=> res.json()).then(res => this.setState({textForPost: res.text}));
+  }
+
+  changeUserInfo = () => {
+    this.state.textOfPutTest.trim() &&
+     fetch('/api/test',{
+      method: 'PUT',
+      headers:{
+        'Content-Type': 'application/json',
+         'Accept': 'application/json',
+      },
+      body: JSON.stringify({text: this.state.textOfPutTest})
+    }).then(res=>res.json()).then(res => this.setState({textForPut: res.text}));
+  }
+
+  deleteUserInfo = () => {
+    this.state.textOfDeleteTest.trim() &&
+    fetch('/api/test',{
+      method: 'DELETE',
+      headers:{
+        'Content-Type': 'application/json',
+         'Accept': 'application/json',
+      },
+      body: JSON.stringify({text: this.state.textOfDeleteTest})
+    }).then(res=>res.json()).then(res => this.setState({textForDelete: res.text}));
   }
 
   render() {
-    const { username } = this.state;
+    const { username, textForPost, textForPut, textForDelete } = this.state;
+    const inputText = "Input text...";
     return (
       <div>
         <div>
-          <button onClick={this.getUser}>
-            Say Hi
-          </button>
+        <div>
+        <div>
+        <button onClick={this.getUser}>{"Test Get"}</button>
+        </div>
+        <label>{"Test for Get: "}</label>
+        <h2>{!!username && `Hello ${username}!`}</h2>
         </div>
         <div>
-          {username &&
-          <React.Fragment>
-          <h1>{`Hello ${username.toUpperCase()}`}</h1>
-          <img src={ProfilePicture} />
-          </React.Fragment> 
-          }
+          <input onChange={e => this.setState({ textOfPostTest: e.target.value })} placeholder={inputText}/>
+          <button onClick={this.sendUserInfo}>{"Test Post"}</button>
+          </div>
+          <div>
+          <label>{"Test for Post: "}</label>
+          <h3>{textForPost}</h3>
+          </div>
+          <div>
+            <input onChange={e => this.setState({textOfPutTest: e.target.value})} placeholder={inputText}/>
+            <button onClick={this.changeUserInfo} >{"Test Put"}</button>
+          </div>
+          <div>
+            <label>{"Put text test: "}</label>
+            <h3>{textForPut}</h3>
+          </div>
+          <div>
+            <input onChange={e => this.setState({textOfDeleteTest: e.target.value})} placeholder={inputText}/>
+            <button onClick={this.deleteUserInfo} >{"Test Delete"}</button>
+          </div>
+          <div>
+            <label>{"Delete text test: "}</label>
+            <h3>{textForDelete}</h3>
+          </div>
         </div>
       </div>
     );
